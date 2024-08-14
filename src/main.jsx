20ec368f624c975856372df48c62124bb7bcfcef
Md Ain Uddin Ahmmad <real.world.developer@gmail.com>
2024-08-14 11:40:35 +0100

    forth commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './Redux/Store/Store.jsx'
 import {Provider} from 'react-redux'
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store= {store}>
     <App />
   </Provider>,
 )
-reportWebVitals()
